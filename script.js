eba38566fe278a1740a973ac38c2f60ecac0788c
red wolf <myash7535@gmail.com>
2024-03-19 10:32:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = 5
+let b = 10
+let c = a
+a=b
+b=c
+console.log(a);
+console.log(b);      
+
+// with using let (c)
+
+// let a = 5
+// let b = 10
+// a=b
+// b=a
+// console.log(a);
+// console.log(b);
