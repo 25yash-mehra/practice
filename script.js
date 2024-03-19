1fe736846fccbc3474f95e9e802379fc21ce48d9
red wolf <myash7535@gmail.com>
2024-03-19 16:49:03 +0530

    variables play done quiz

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let a = 5
-let b = 10
-let c = a
-a=b
-b=c
-console.log(a);
-console.log(b);      
+// let a = 5
+// let b = 10
+// let c = a
+// a=b
+// b=c
+// console.log(a);
+// console.log(b);
 
 // with using let (c)
 
@@ -14,3 +14,99 @@ console.log(b);
 // b=a
 // console.log(a);
 // console.log(b);
+
+// let a = 5
+// let b = 10
+// let c = a
+// a=b
+// b=c
+// console.log(a);
+// console.log(b);
+
+// let a = 5
+// let b = 10
+// let c = a
+// a=b
+// b=c
+// console.log(a,b);
+// let a = 5
+// let b = 10
+// a = (a+b) // 5+10 = 15 // a value = 15 // code executed
+// b = (a-b) // 15-10 = 5 // b value = 5 // code executed
+// a = (a-b) // 5-10 = 15 // 10
+
+// console.log(a);
+// console.log(b);
+
+// let a = 5;
+// let b = 10;
+// // a=10 b=5
+// let c = 15;
+// let d = 20;
+// // c=15 d=20
+// let temp = a; // temp me aab a ke value aa gaye he to temp aab 5
+// a = b; // a me aab b ke value aa gaye he a=10
+// b = temp; // b me temp ke value aa gaye he temp me a ke value the 5 jo ab be me aa gaye he 5
+// temp = c;
+// c = d;
+// d = temp;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// let a = 5
+// let b = 10
+// a = (a+b) // isme aab total ata he 15 to a=15
+// a ka code execute done aab b ke bare
+// b = (a-b)
+// b me hum essa kya kare jo 5 aaa jaye 
+// a=15 or b = 10 plus karnege to 25 hoga par apan ko 5 chye to minus 
+// a me aa gaya he 15 or b me aa gaya he 5 
+// aab a me apn ko chay 10 to iska be koi hal nikalo
+// to firse ek statement run hoge 
+// a = (a-b)
+// aab a me essa kya kare jo 10 aa jaye 
+// a he 15  or b  he 5 
+// aab plus karnge to hoga 20 
+// to minus karo 15-5
+// console.log(a);
+// console.log(b);
+
+let a = 10
+let b = 15
+// a=15 b=10
+let c = 20
+let d = 25
+// c=25 d=20
+ 
+a = (a+b)
+// 10+15 = 25
+// aab next line pe aate he vo he B
+// a ke value he aab 25
+b = (a-b)
+// b me aab plus karenege to jayada aayega 
+// a ke value he aab 25 or b ke he 15 25+15=40 par apan ko to 10 chayea
+// 25-15 = 10
+// B me to aab aa gaya he 15 par a me abhe be 25 he 
+// aab A ka kuch statement karna padega 
+a = (a-b)
+// a he 25 or b he 15
+//25-10=15
+// isme B ko 15 assign isliye nahe kiya aab b ,me humne new value assign kiya he jo he 10
+c = (c+d)
+// 20+25 = 45
+// console.log(20+25);
+ d = (c-d) 
+ //c he 45 or d he 25
+ //45-25 = 25 
+ //aab 20 he B ke value aab vo 25 nahe he 
+//  console.log(45-25);
+ c = (c-d)
+ //45-20=25
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
